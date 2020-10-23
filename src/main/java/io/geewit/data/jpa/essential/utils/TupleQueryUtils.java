115c41f56a4e69659d6348f217382f889bb9342c
geewit <hf@geewit.io>
2020-10-23 22:11:53 +0800

    exclude hibernate-envers

--- src/main/java/io/geewit/data/jpa/essential/utils/TupleQueryUtils.java
+++ src/main/java/io/geewit/data/jpa/essential/utils/TupleQueryUtils.java
@@ -4,10 +4,8 @@ import org.springframework.util.Assert;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -19,27 +17,43 @@ public class TupleQueryUtils {
     /**
      * Executes a count query and transparently sums up all values returned.
      *
-     * @param entityManager must not be {@literal null}.
+     * @param query must not be {@literal null}.
      * @return
      */
-    public static <T> long executeCountQuery(EntityManager entityManager, Root<T> root, Predicate predicate) {
+    public static long executeCountQuery(TypedQuery<Long> query) {
 
-        Assert.notNull(entityManager, "entityManager must not be null!");
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Long> criteriaQuery = criteriaBuilder.createQuery(Long.class);
-        criteriaQuery.where(predicate);
-        if (criteriaQuery.isDistinct()) {
-            criteriaQuery.select(criteriaBuilder.countDistinct(root));
-        } else {
-            criteriaQuery.select(criteriaBuilder.count(root));
-        }
-        TypedQuery<Long> criteriaCountQuery = entityManager.createQuery(criteriaQuery);
-        List<Long> totals = criteriaCountQuery.getResultList();
+        Assert.notNull(query, "TypedQuery must not be null!");
+
+        List<Long> totals = query.getResultList();
         long total = 0L;
+
         for (Long element : totals) {
             total += element == null ? 0 : element;
         }
 
         return total;
+    }
+
+    /**
+     * Creates a new count query for the given {@link EntityManager}.
+     *
+     * @param entityManager can be {@literal null}.
+     * @param domainClass must not be {@literal null}.
+     * @return
+     */
+    public static <T> TypedQuery<Long> getCountQuery(EntityManager entityManager, Class<T> domainClass, CriteriaQuery<Long> criteriaQuery, CriteriaBuilder criteriaBuilder, Predicate predicate) {
+
+        Assert.notNull(entityManager, "entityManager must not be null!");
+        Root<T> root = criteriaQuery.from(domainClass);
+        if (criteriaQuery.isDistinct()) {
+            criteriaQuery.select(criteriaBuilder.countDistinct(root));
+        } else {
+            criteriaQuery.select(criteriaBuilder.count(root));
+        }
+        criteriaQuery.where(predicate);
+        criteriaQuery.orderBy(Collections.emptyList());
+        TypedQuery<Long> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        return typedQuery;
     }
 }
