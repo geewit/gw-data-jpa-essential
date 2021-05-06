4bb1d2177aa51e9967e4d78988657e06f836a4e0
黄方 <hf@geewit.io>
2021-05-06 18:01:55 +0800

    exclude hibernate-envers

---
+++ src/main/java/io/geewit/data/jpa/essential/dialect/GeewitMySQL57Dialect.java
@@ -0,0 +1,22 @@
+package io.geewit.data.jpa.essential.dialect;
+
+import io.geewit.data.jpa.essential.function.Functions;
+import org.hibernate.dialect.MySQL57Dialect;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.type.StandardBasicTypes;
+
+/**
+ * 自定义 Mysql 5.7 方言
+ * @author geewit
+ */
+public class GeewitMySQL57Dialect extends MySQL57Dialect {
+    public GeewitMySQL57Dialect() {
+        super();
+        super.registerFunction(Functions.BITWISE_AND, new SQLFunctionTemplate(StandardBasicTypes.INTEGER,
+                "(?1 & ?2)"));
+        super.registerFunction(Functions.BITWISE_OR, new SQLFunctionTemplate(StandardBasicTypes.INTEGER,
+                "(?1 | ?2)"));
+        super.registerFunction(Functions.BITWISE_XOR, new SQLFunctionTemplate(StandardBasicTypes.INTEGER,
+                "(?1 ^ ?2)"));
+    }
+}
---
+++ src/main/java/io/geewit/data/jpa/essential/function/Functions.java
@@ -0,0 +1,22 @@
+package io.geewit.data.jpa.essential.function;
+
+/**
+ * 自定义函数
+ * @author geewit
+ */
+public interface Functions {
+    /**
+     * 按位与运算
+     */
+    String BITWISE_AND = "bitwiseAnd";
+
+    /**
+     * 按位或运算
+     */
+    String BITWISE_OR = "bitwiseOr";
+
+    /**
+     * 按位异或运算
+     */
+    String BITWISE_XOR = "bitwiseXor";
+}
